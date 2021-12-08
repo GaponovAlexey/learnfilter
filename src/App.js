7b187d1b6666318e51ad8ba2ff5afd348363eb18
kekelberi <alexei.gaponov@gmail.com>
2021-12-08 13:33:36 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState([
+    { id: 1, title: 'java', body: 'history' },
+    { id: 2, title: 'javascript', body: 'favorite' },
+    { id: 3, title: 'Go', body: 'feature' },
+    { id: 4, title: 'Rast', body: 'good' },
+    { id: 5, title: 'C++', body: 'legacy' },
+    { id: 6, title: 'C#', body: 'game' },
+    { id: 7, title: 'Kotlin', body: 'android' },
+    { id: 8, title: 'Ryby', body: 'functional' },
+    { id: 8, title: 'Python', body: 'good' },
+    { id: 8, title: 'basic', body: 'legacy' },
+  ])
+
   return (
-    <div className="App">
-    start
+    <div>
+      <div>
+        <input placeholder="search"  />
+      </div>
+      <div>
+        {data.map((el) => (
+          <h1>
+            {el.title} - {el.body}
+          </h1>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
