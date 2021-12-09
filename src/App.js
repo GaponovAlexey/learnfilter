4ed315c6443abc4e9ca9957a127e9057983b1520
kekelberi <alexei.gaponov@gmail.com>
2021-12-09 13:07:38 +0300

    save

--- src/App.js
+++ src/App.js
@@ -10,55 +10,73 @@ function App() {
     { id: 6, title: 'C#', body: 'game' },
     { id: 7, title: 'Kotlin', body: 'android' },
     { id: 8, title: 'Ryby', body: 'functional' },
-    { id: 8, title: 'Python', body: 'good' },
-    { id: 8, title: 'basic', body: 'legacy' },
+    { id: 9, title: 'Python', body: 'good' },
+    { id: 10, title: 'basic', body: 'legacy' },
   ])
-
-  const [opti, setOnpion] = useState([
-    { value: 'title', name: 'по названию' },
-    { value: 'body', name: 'по тексту' },
+  const [options, setOptions] = useState([
+    { value: 'title', name: 'по заголовку' },
+    { value: 'body', name: 'по телу' },
   ])
-
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
   const [select, setSelect] = useState('')
   const [query, setQuery] = useState('')
+  console.log('da')
 
-  const selectChoice = useMemo(() => {
+  const selectSearch = useMemo(() => {
     if (select) {
-      return [...data].sort((a, b) => a[select].localeComplare(b[select]))
+      return [...data].sort((a, b) => a[select].localeCompare(b[select]))
     }
     return data
-  }, [data, select])
+  }, [select, data])
+  const querySearch = useMemo(() => {
+    return selectSearch.filter((el) => el.title.toLowerCase().includes(query))
+  }, [query, selectSearch])
 
-  const queryChoice = useMemo(() => {
-    return selectChoice.filter((el) => el.title.toLowerCase().includes(query))
-  }, [query, selectChoice])
-
+  const addData = () => {
+    const newData = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setData([ newData, ...data])
+  }
   return (
     <div>
-      <div>
-        <input
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder='search'
-        />
-        <hr />
-        <select value={select} onChange={(e) => setSelect(e.target.value)}>
-          <option disabled>выбор</option>
-          {opti.map((el) => (
-            <option key={el.value} value={el.value}>
-              {el.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      {data.length ? (
-        queryChoice.map((el) => (
-          <h2>
-            {el.title} - {el.body}
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder='search'
+      />
+      <hr />
+      <input
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type='text'
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      <button onClick={addData}>send</button>
+      <hr />
+      <select value={select} onChange={(el) => setSelect(el.target.value)}>
+        <option disabled>поиск</option>
+        {options.map((el) => (
+          <option key={el.value} value={el.value}>
+            {el.name}
+          </option>
+        ))}
+      </select>
+      {data ? (
+        querySearch.map((el, index) => (
+          <h2 key={el.id}>
+            {index + 1}.{el.title} - {el.body}
           </h2>
         ))
       ) : (
-        <h1>Ничего нет</h1>
+        <h1>ничего нет</h1>
       )}
     </div>
   )
